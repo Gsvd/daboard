b11b3ac0ce16ae7cbd1a06ac7cd015fccbc7c50c
Velkow <sivade.g@gmail.com>
2018-11-18 12:09:35 +0100

    admin view and skeleton css

---
+++ app/src/views/Admin.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="content">
+    <Navbar/>
+    <div class="frame">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  name: 'admin',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+
+  .frame {
+    
+    flex-grow: 1;
+
+  }
+
+}
+</style>
