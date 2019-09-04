3aca4cec5010d318ef992bbe97438dc20436d78a
Guillaume SIVADE <guillaume.sivade@capgemini.com>
2019-09-04 10:31:21 +0200

    Fix syntax error

--- api/db.js
+++ api/db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  charset : 'utf8mb4'
+  charset: 'utf8mb4'
 })
 
 module.exports = db
