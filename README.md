eacfa64517269c0fe9cafc3a591c17218f4a7a08
Velkow <sivade.g@gmail.com>
2019-11-28 18:13:53 +0100

    change readme and sql script generation

--- README.md
+++ README.md
@@ -6,6 +6,21 @@ cd api && npm i
 cd app && npm i
 ```
 
+Create ``api/.env`` file
+
+```
+DB_HOST=
+DB_USER=
+DB_PASSWORD=
+DP_PORT=
+DB_DATABASE=
+
+OAUTH_ID=
+OAUTH_SECRET=
+OAUTH_USERNAME=
+OAUTH_PASSWORD=
+```
+
 ### Compiles and hot-reloads for development
 ```
 cd api && npm start
--- db/daboard.sql
+++ db/daboard.sql
@@ -1,143 +1,41 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Mar 28, 2019 at 08:25 PM
--- Server version: 5.7.25
--- PHP Version: 7.2.14
+create table categories
+(
+    id    int auto_increment
+        primary key,
+    value varchar(16) not null
+)
+    charset = utf8mb4;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+create table posts
+(
+    id       int auto_increment
+        primary key,
+    title    varchar(255) not null,
+    content  text         not null,
+    html     text         not null,
+    author   int          not null,
+    category varchar(16)  not null,
+    creation datetime     not null
+)
+    charset = utf8mb4;
 
---
--- Database: `daboard`
---
-CREATE DATABASE IF NOT EXISTS `daboard` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-USE `daboard`;
+create table ranks
+(
+    id    int auto_increment
+        primary key,
+    label varchar(255) not null
+)
+    charset = utf8mb4;
 
--- --------------------------------------------------------
+create table users
+(
+    id       int auto_increment
+        primary key,
+    username varchar(255) not null,
+    password text         not null,
+    token    text         null,
+    `rank`   tinyint      not null,
+    creation datetime     not null
+)
+    charset = utf8mb4;
 
---
--- Table structure for table `categories`
---
-
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `value` varchar(16) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `value`) VALUES
-(1, 'SIS'),
-(2, 'BDE'),
-(3, 'DEV');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `posts`
---
-
-CREATE TABLE `posts` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `html` text NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `category` varchar(16) NOT NULL,
-  `creation` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ranks`
---
-
-CREATE TABLE `ranks` (
-  `id` int(11) NOT NULL,
-  `label` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `ranks`
---
-
-INSERT INTO `ranks` (`id`, `label`) VALUES
-(1, 'Admin'),
-(2, 'User');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `password` text NOT NULL,
-  `token` text,
-  `rank` tinyint(4) NOT NULL,
-  `creation` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `posts`
---
-ALTER TABLE `posts`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `ranks`
---
-ALTER TABLE `ranks`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `posts`
---
-ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `ranks`
---
-ALTER TABLE `ranks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
